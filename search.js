05715ab68f96ffa6245d7150cf1d21980f1ed4ec
eurovision-survey <blibio0074@gmail.com>
2025-05-24 10:48:54 +0200

    Update search.js

--- search.js
+++ search.js
@@ -5,8 +5,8 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 import { renderCoincidencias, renderGoogleResults, showAlert, showError } from './ui.js';
 
 // Inicializa el cliente de Supabase
-const SUPABASE_URL = 'https://tusupabaseurl.supabase.co';
-const SUPABASE_KEY = 'tusupabaseanonkey';
+const SUPABASE_URL = 'https://vrbheaswtkheyxswnhrp.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyYmhlYXN3dGtoZXl4c3duaHJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4MjkzMDcsImV4cCI6MjA2MDQwNTMwN30.3lrx_kJwp7uHbhu9IgKGTM5Somobi4tjTiYdCtEYW1o';
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 /**
